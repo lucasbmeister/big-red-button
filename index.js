dabfaad7d85688de594c4a666873f20a2f5da4ff
lucasbmeister <lucas_bmeister@hotmail.com>
2022-09-22 10:32:36 -0300

    width

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById("button");
 
     function moveButton() {
-        let randX = Math.floor(Math.random() * (window.innerWidth - 100));
+        let randX = Math.floor(Math.random() * (window.innerWidth - 480));
         button.animate([
             { transform: "scale(0.8)", "left": randX + "px" },
             { transform: "scale(1.2)", "bottom": 100 + "px" }
