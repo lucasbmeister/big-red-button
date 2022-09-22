64bc7d90e844578fde9d66d7a533c6cd5ecd8dcd
lucasbmeister <lucas_bmeister@hotmail.com>
2022-09-22 10:25:19 -0300

    wait load

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById("button");
 
-const button = document.getElementById("button");
 
+    let count = 0;
 
-let count = 0;
-
-function moveButton() {
-    count++;
-    let randX = Math.floor(Math.random() * (window.innerWidth - 100));
-    if (count < 10) {
-        button.animate([
-            { transform: "scale(0.8)", "left": randX + "px" },
-            { transform: "scale(1.2)", "bottom": 100 + "px" }
-        ], { duration: 2000 })
-    } else {
-        button.animate([
-            { "left": randX + "px" },
-            { "bottom": 100 + "px" },
-        ], { duration: 5000 })
+    function moveButton() {
+        count++;
+        let randX = Math.floor(Math.random() * (window.innerWidth - 100));
+        if (count < 10) {
+            button.animate([
+                { transform: "scale(0.8)", "left": randX + "px" },
+                { transform: "scale(1.2)", "bottom": 100 + "px" }
+            ], { duration: 2000 })
+        } else {
+            button.animate([
+                { "left": randX + "px" },
+                { "bottom": 100 + "px" },
+            ], { duration: 5000 })
+        };
     };
-};
 
 
-button.addEventListener('mouseenter', moveButton);
+    button.addEventListener('mouseenter', moveButton);
+})
+
