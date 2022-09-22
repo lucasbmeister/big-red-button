5f3b3fc13cba2d2cd246f80eeedc81dc80b490dc
lucasbmeister <lucas_bmeister@hotmail.com>
2022-09-22 10:29:31 -0300

    fix move

--- index.js
+++ index.js
@@ -2,25 +2,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById("button");
 
-
-    let count = 0;
-
     function moveButton() {
-        count++;
         let randX = Math.floor(Math.random() * (window.innerWidth - 100));
-        if (count < 10) {
-            button.animate([
-                { transform: "scale(0.8)", "left": randX + "px" },
-                { transform: "scale(1.2)", "bottom": 100 + "px" }
-            ], { duration: 2000 })
-        } else {
-            button.animate([
-                { "left": randX + "px" },
-                { "bottom": 100 + "px" },
-            ], { duration: 5000 })
-        };
+        button.animate([
+            { transform: "scale(0.8)", "left": randX + "px" },
+            { transform: "scale(1.2)", "bottom": 100 + "px" }
+        ], { duration: 2000 })
     };
-
 
     button.addEventListener('mouseenter', moveButton);
 })
